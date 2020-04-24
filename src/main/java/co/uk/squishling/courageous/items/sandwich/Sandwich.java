a285399e90f06ae68f13f00b8f8fdba3ad57f854
Squishling <null@example.com>
2020-04-25 03:55:03 +0400

    more stuff that works but doesn't sync to the client for SANDWICHES

--- src/main/java/co/uk/squishling/courageous/items/sandwich/Sandwich.java
+++ src/main/java/co/uk/squishling/courageous/items/sandwich/Sandwich.java
@@ -9,28 +9,29 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.ListNBT;
+import net.minecraft.nbt.INBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.capabilities.ICapabilitySerializable;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
-import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class Sandwich extends ItemBase {
+public class Sandwich extends ItemBase implements ICapabilityProvider, ICapabilitySerializable {
 
-    private LazyOptional<ItemStackHandler> handler = LazyOptional.of(() -> getHandler()).cast();
+    private LazyOptional<IItemHandler> handler;
 
     public Sandwich() {
         super("sandwich", new Item.Properties().group(FoodTab.FOOD).setISTER(() -> SandwichISTER::new));
+        handler =  LazyOptional.of(() -> new ItemStackHandler(4));
     }
 
     @Override
@@ -40,35 +41,41 @@ public class Sandwich extends ItemBase {
         return super.onItemRightClick(worldIn, playerIn, handIn);
     }
 
-    @Nullable
+    @Nonnull
     @Override
-    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT nbt) {
-        return new ICapabilityProvider() {
-            @Nonnull
-            @Override
-            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-                if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && stack.hasTag() && stack.getTag().get("handler") != null) {
-                    ItemStackHandler h = new ItemStackHandler();
-                    h.deserializeNBT((CompoundNBT) stack.getTag().get("handler"));
-                    return LazyOptional.of(() -> h).cast();
-                }
-                return LazyOptional.empty();
-            }
-        };
-    }
+    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return handler.cast();
 
-    private ItemStackHandler getHandler() {
-        return new ItemStackHandler(4);
+        return LazyOptional.empty();
     }
 
     public void addIngredient(ItemStack stack, ItemStack ingredient) {
         stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
             for (int i = 0; i < h.getSlots(); i++) if (h.getStackInSlot(i).isEmpty()) {
                 h.insertItem(i, ingredient, false);
-                stack.getOrCreateTag().put("handler", ((ItemStackHandler) h).serializeNBT());
+                write();
                 break;
             }
         });
+    }
+
+    private CompoundNBT write() {
+        CompoundNBT nbt = (CompoundNBT) handler.map(h -> CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.writeNBT(h, null)).orElseGet(CompoundNBT::new);
+        return nbt;
+    }
+
+    private void read(CompoundNBT nbt) {
+        handler.ifPresent(h -> CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.readNBT(h, null, nbt));
+    }
+
+    @Override
+    public INBT serializeNBT() {
+        return write();
+    }
+
+    @Override
+    public void deserializeNBT(INBT nbt) {
+        read((CompoundNBT) nbt);
     }
 
 }
--- src/main/java/co/uk/squishling/courageous/items/sandwich/SandwichISTER.java
+++ src/main/java/co/uk/squishling/courageous/items/sandwich/SandwichISTER.java
@@ -12,8 +12,6 @@ import net.minecraftforge.items.CapabilityItemHandler;
 
 public class SandwichISTER extends ItemStackTileEntityRenderer {
 
-    private int x = 0;
-
     @Override
     public void render(ItemStack stack, MatrixStack matrix, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
         super.render(stack, matrix, bufferIn, combinedLightIn, combinedOverlayIn);
@@ -22,25 +20,19 @@ public class SandwichISTER extends ItemStackTileEntityRenderer {
         matrix.translate(1f, 1f, 0.8f);
 
         Minecraft.getInstance().getItemRenderer().renderItem(new ItemStack(ModItems.BREAD_SLICE), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
-        matrix.scale(0.8f, 0.8f, 1f);
 
         stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
             for (int i = 0; i < h.getSlots(); i++) {
 
                 if (!h.getStackInSlot(i).isEmpty()) {
-                    x++;
-                    if (x >= 200) {
-                        System.out.println(h.getStackInSlot(i).getItem().getRegistryName());
-                        x = 0;
-                    }
                     matrix.translate(0f, 0f, 0.06f);
                     Minecraft.getInstance().getItemRenderer().renderItem(h.getStackInSlot(i), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
                 }
+
             }
         });
 
         matrix.translate(0f, 0f, 0.06f);
-        matrix.scale(1.2f, 1.2f, 1f);
         Minecraft.getInstance().getItemRenderer().renderItem(new ItemStack(ModItems.BREAD_SLICE), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
 
         matrix.pop();
