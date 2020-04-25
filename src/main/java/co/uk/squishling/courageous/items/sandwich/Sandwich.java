60fdc8ee2ba9e3cf828a70f65c5e5575f890a985
Squishling <null@example.com>
2020-04-26 02:17:39 +0400

    almost working sandwich

--- src/main/java/co/uk/squishling/courageous/items/sandwich/Sandwich.java
+++ src/main/java/co/uk/squishling/courageous/items/sandwich/Sandwich.java
@@ -3,6 +3,7 @@ package co.uk.squishling.courageous.items.sandwich;
 import co.uk.squishling.courageous.items.ItemBase;
 import co.uk.squishling.courageous.items.ModItems;
 import co.uk.squishling.courageous.tabs.FoodTab;
+import co.uk.squishling.courageous.util.Reference;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -11,13 +12,17 @@ import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.INBT;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.fluids.capability.templates.FluidHandlerItemStack;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
@@ -25,57 +30,69 @@ import net.minecraftforge.items.ItemStackHandler;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class Sandwich extends ItemBase implements ICapabilityProvider, ICapabilitySerializable {
+public class Sandwich extends ItemBase {
 
-    private LazyOptional<IItemHandler> handler;
+    private String HANDLER = "handler";
 
     public Sandwich() {
         super("sandwich", new Item.Properties().group(FoodTab.FOOD).setISTER(() -> SandwichISTER::new));
-        handler =  LazyOptional.of(() -> new ItemStackHandler(4));
     }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         ItemStack stack = playerIn.getHeldItem(handIn);
-        addIngredient(stack, new ItemStack(Items.APPLE));
-        return super.onItemRightClick(worldIn, playerIn, handIn);
+        if (Reference.isServer(worldIn))
+        addIngredient(stack, handIn == Hand.MAIN_HAND ? playerIn.getHeldItemOffhand() : playerIn.getHeldItemMainhand());
+        return new ActionResult<>(ActionResultType.SUCCESS, stack);
     }
 
-    @Nonnull
+    @Nullable
     @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) return handler.cast();
+    public ICapabilityProvider initCapabilities(ItemStack stack, @Nullable CompoundNBT nbt) {
+        return new ICapabilityProvider() {
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+                if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+                    if (stack.hasTag() && stack.getTag().contains(HANDLER, Constants.NBT.TAG_COMPOUND)) {
+                        ItemStackHandler h = new ItemStackHandler();
+                        h.deserializeNBT((CompoundNBT) stack.getTag().get(HANDLER));
+                        return LazyOptional.of(() -> h).cast();
+                    } else {
+                        return LazyOptional.of(() -> new ItemStackHandler(4) {
+                            @Override
+                            protected void onContentsChanged(int slot) {
+                                stack.getOrCreateTag().put(HANDLER, this.serializeNBT());
+                            }
 
-        return LazyOptional.empty();
+                            @Override
+                            public int getSlotLimit(int slot) {
+                                return 1;
+                            }
+                        }).cast();
+                    }
+                }
+
+                return LazyOptional.empty();
+            }
+        };
     }
 
     public void addIngredient(ItemStack stack, ItemStack ingredient) {
         stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
-            for (int i = 0; i < h.getSlots(); i++) if (h.getStackInSlot(i).isEmpty()) {
-                h.insertItem(i, ingredient, false);
-                write();
-                break;
+            ItemStack leftover = ingredient.copy();
+            for (int i = 0; i < h.getSlots(); i++) {
+                System.out.println(leftover);
+                leftover = h.insertItem(i, leftover, false);
+                if (leftover.isEmpty()) break;
             }
         });
-    }
 
-    private CompoundNBT write() {
-        CompoundNBT nbt = (CompoundNBT) handler.map(h -> CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.writeNBT(h, null)).orElseGet(CompoundNBT::new);
-        return nbt;
-    }
-
-    private void read(CompoundNBT nbt) {
-        handler.ifPresent(h -> CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.readNBT(h, null, nbt));
-    }
-
-    @Override
-    public INBT serializeNBT() {
-        return write();
-    }
-
-    @Override
-    public void deserializeNBT(INBT nbt) {
-        read((CompoundNBT) nbt);
+        stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
+            for (int i = 0; i < h.getSlots(); i++) {
+                System.out.println(h.getStackInSlot(i));
+            }
+        });
     }
 
 }
--- src/main/java/co/uk/squishling/courageous/items/sandwich/SandwichISTER.java
+++ src/main/java/co/uk/squishling/courageous/items/sandwich/SandwichISTER.java
@@ -4,11 +4,15 @@ import co.uk.squishling.courageous.items.ModItems;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.model.ItemCameraTransforms.TransformType;
 import net.minecraft.client.renderer.tileentity.ItemStackTileEntityRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class SandwichISTER extends ItemStackTileEntityRenderer {
 
@@ -17,16 +21,18 @@ public class SandwichISTER extends ItemStackTileEntityRenderer {
         super.render(stack, matrix, bufferIn, combinedLightIn, combinedOverlayIn);
         matrix.push();
 
-        matrix.translate(1f, 1f, 0.8f);
+        matrix.translate(0.5f, 0.5f, 0.5f);
+        matrix.rotate(Vector3f.XP.rotationDegrees(90));
+        matrix.rotate(Vector3f.YN.rotationDegrees(90));
 
         Minecraft.getInstance().getItemRenderer().renderItem(new ItemStack(ModItems.BREAD_SLICE), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
 
-        stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(h -> {
-            for (int i = 0; i < h.getSlots(); i++) {
+        stack.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY).ifPresent(handler -> {
+            for (int i = 0; i < handler.getSlots(); i++) {
 
-                if (!h.getStackInSlot(i).isEmpty()) {
+                if (!handler.getStackInSlot(i).isEmpty()) {
                     matrix.translate(0f, 0f, 0.06f);
-                    Minecraft.getInstance().getItemRenderer().renderItem(h.getStackInSlot(i), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
+                    Minecraft.getInstance().getItemRenderer().renderItem(handler.getStackInSlot(i), TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrix, bufferIn);
                 }
 
             }
